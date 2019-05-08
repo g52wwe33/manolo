a568f3fd49d8c6ffa13085e7a8c9f6561ff0fa54
g52wwe33 <javiercalzadabelarra@gmail.com>
2019-05-08 12:25:45 +0200

    Inicio del proyecto

--- src/manolo/main.java
+++ src/manolo/main.java
@@ -3,7 +3,8 @@ package manolo;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Esto e una 36prueba Aitor es thor");
+		System.out.println("Esto e una 37rueba Javie es spiderman");
 
 	}
 
